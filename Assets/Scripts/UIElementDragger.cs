24eb942a4e1ae9d9ecc664f7dd227c4187f7c227
Paritosh <contact@iamparitoshsharma.me>
2019-01-25 20:28:27 +0530

    Improved drag and drop and UI

---
+++ Assets/Scripts/UIElementDragger.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class UIElementDragger : MonoBehaviour
+{
+
+    public const string DRAGGABLE_TAG = "UIDraggable";
+
+    private bool dragging = false;
+
+    private Vector2 originalPosition;
+    private Transform objectToDrag;
+    private Image objectToDragImage;
+
+    List<RaycastResult> hitObjects = new List<RaycastResult>();
+
+    #region Monobehaviour API
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            objectToDrag = GetDraggableTransformUnderMouse();
+
+            if (objectToDrag != null)
+            {
+                dragging = true;
+
+                objectToDrag.SetAsLastSibling();
+
+                originalPosition = objectToDrag.position;
+                objectToDragImage = objectToDrag.GetComponent<Image>();
+                objectToDragImage.raycastTarget = false;
+            }
+        }
+
+        if (dragging)
+        {
+            objectToDrag.position = Input.mousePosition;
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            if (objectToDrag != null)
+            {
+                var objectToReplace = GetDraggableTransformUnderMouse();
+
+                if (objectToReplace != null && (objectToReplace.name != "Crest" && objectToReplace.name != "Ascent" && objectToReplace.name != "Eagles" && objectToReplace.name != "Kinley"))
+                {
+                    objectToDrag.position = objectToReplace.position;
+                    objectToDrag.SetParent(objectToReplace);
+                }
+                else
+                {
+                    objectToDrag.position = originalPosition;
+                }
+
+                objectToDragImage.raycastTarget = true;
+                objectToDrag = null;
+            }
+
+            dragging = false;
+        }
+    }
+
+    private GameObject GetObjectUnderMouse()
+    {
+        var pointer = new PointerEventData(EventSystem.current);
+
+        pointer.position = Input.mousePosition;
+
+        EventSystem.current.RaycastAll(pointer, hitObjects);
+
+        if (hitObjects.Count <= 0) return null;
+
+        return hitObjects.First().gameObject;
+    }
+
+    private Transform GetDraggableTransformUnderMouse()
+    {
+        var clickedObject = GetObjectUnderMouse();
+
+        // get top level object hit
+        if (clickedObject != null && clickedObject.tag == DRAGGABLE_TAG)
+        {
+            return clickedObject.transform;
+        }
+
+        return null;
+    }
+
+    #endregion
+}
